515f969de50b4027c8707a504bc675d9e9b25332
Akhil Batra <85938386+batraakhil22@users.noreply.github.com>
2022-03-10 09:17:54 +0530

    Add files via upload

---
+++ seo-main/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+import Seo from "./components/Seo";
+
+function App() {
+  return (
+    <div className="App">
+      <Seo />
+    </div>
+  );
+}
+
+export default App;
